f50e86c255e9b71e85d037faf53789048e75e27a
Mihalnaa <radugaduga95@gmail.com>
2022-07-12 12:42:14 +0300

    add py file

--- kitten_task.py
+++ kitten_task.py
@@ -1,16 +1,14 @@
-# -Python
-#Задача:
-#Введите два числа и выведите котиков
-b,c=[int(i) for i in input().split()]
-a=[]
+# Задача:
+# Введите два числа и выведите котиков
+b, c = [int(i) for i in input().split()]
+a = []
 print('Котики:')
 for i in range(b):
     a.append([])
     for j in range(c):
-        if (i+j)%2==0:
+        if (i + j) % 2 == 0:
             a[i].append('^')
         else:
             a[i].append('__')
 for d in a:
     print(''.join(d))
-    
